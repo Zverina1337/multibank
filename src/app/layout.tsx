8db1617c74b6e797b0b478c6a23c2722ee20e1a1
Zverina1337 <danii.zvorugin@gmail.com>
2025-11-02 15:39:46 +0700

    fix: configure Tailwind CSS and build mistakes

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -1,12 +1,12 @@
 import type { Metadata } from "next";
 import { Inter } from "next/font/google"
+import "./globals.css";
 
 const inter = Inter({
   subsets: ["latin"],
   variable: "--font-sans",
 })
 
-import "./globals.css";
 
 export const metadata: Metadata = {
   title: "Create Next App",
